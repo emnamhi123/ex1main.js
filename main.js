1dc4ea05ce6b9e3c6638abb7b2078447e4d48935
emnamhi123 <emna-mhiri@hotmail.com>
2023-10-10 16:03:16 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let num1 = 2;
+let num2 = 5;
+let test1 = num1 % 2;
+let test2 = num2 % 2;
+result1 = test1 == 0;
+result2 = test2 == 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
+
+///////////////////////////////////////////
+
+let now = "Now in";
+let three = 3;
+let d = "D!";
+console.log(`${now} ${three}${d}`);
+
+////////////////////////////////////////
+
+let counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter)
